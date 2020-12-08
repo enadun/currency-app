34c7c608e304a980a9d6bb17c2deb5290e900f9d
Nadun De Silva <enadun@yahoo.com>
2020-12-08 23:51:16 +0800

    Basic UI created.

---
+++ currency app paypay/Main Screen/Cell/ExchangeCell.swift
@@ -0,0 +1,16 @@
+//
+//  ExchangeCell.swift
+//  currency app paypay
+//
+//  Created by Nadun De Silva on 20/Dec/08.
+//  Copyright © 2020 Nadun De Silva. All rights reserved.
+//
+
+import UIKit
+
+class ExchangeCell: UITableViewCell {
+    static let dentifier = "ExchangeCell"
+    @IBOutlet weak var currencyTitleLabel: UILabel!
+    @IBOutlet weak var exchangeRateLabel: UILabel!
+    @IBOutlet weak var exchangeAmountLabel: UILabel!
+}
--- currency app paypay/Main Screen/HomeViewController.swift
+++ currency app paypay/Main Screen/HomeViewController.swift
@@ -9,13 +9,35 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    @IBOutlet weak var currencyTextField: UITextField!
+    @IBOutlet weak var currencySelectionButton: UIButton!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var lastUpdateLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.dataSource = self
+
         // Do any additional setup after loading the view.
 //        CurrencyService.getCurrencyList { result in
 //            print(result)
 //        }
     }
+}
+
+
+extension HomeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ExchangeCell.dentifier,
+                                                 for: indexPath) as? ExchangeCell
+        return cell ?? ExchangeCell()
+    }
+    
+    
 }
 
